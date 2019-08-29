1e8c8646d8ea7ee16cad783b67e3789986efa8e5
sduntz4 <54557840+sduntz4@users.noreply.github.com>
2019-08-29 09:49:48 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@
  * Date: 08/28/2019
  *
  *
- * Print my name and partners name
+ * Print my name and major and my partners name and major
+ *
  */
 #include<stdlib.h>
 #include<stdio.h>
@@ -11,6 +12,8 @@
 int main(int argc, char **argv) {
 
   printf("Spencer Duntz\n");
+
+  printf("Computer Science\n");
 
   return 0;
 }
